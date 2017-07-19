48d46abb79bc8c3b573d29addb84390d971fb5b0
JarkoDubbeldam <jarkodubbeldam@gmail.com>
2017-07-19 12:55:46 +0200

    Added Documentation comments for AppendStrings.

--- R/AppendStrings.R
+++ R/AppendStrings.R
@@ -1,10 +1,28 @@
+#' Addition
+#' 
+#' Modified addition operator to work with strings. 
+#' 
+#' If the first argument (LHS) is of class \code{character}, the two arguments are concatenated and returned as string. Else the base implementation of \code{+} is called.
+#'
+#' @param e1 
+#' @param e2 
+#'
+#' @return A string or numeric.
+#' @export
+#'
+#' @examples
+#' {
+#' 1+1
+#' "a"+"b"
+#' }
 `+` <- function(e1, e2){
   if(is.character(e1)){
-    appendString(e1, e2)}
-  else 
+    paste0(e1, e2)
+  } else {
     .Primitive("+")(e1, e2)
+  }
 }
 
-appendString(e1, e2){
-  paste0(e1, e2)
-}
+
+  
+
