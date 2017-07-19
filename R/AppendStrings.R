b34cf3a65baf96fff891f0acf1840252286fcf26
JarkoDubbeldam <jarkodubbeldam@gmail.com>
2017-07-19 17:53:04 +0200

    Added proper documentation to appendStrings. bump to version 0.1.1.

--- R/AppendStrings.R
+++ R/AppendStrings.R
@@ -1,11 +1,11 @@
 #' Addition
-#' 
-#' Modified addition operator to work with strings. 
-#' 
-#' If the first argument (LHS) is of class \code{character}, the two arguments are concatenated and returned as string. Else the base implementation of \code{+} is called.
 #'
-#' @param e1 
-#' @param e2 
+#' Modified addition operator to work with strings.
+#'
+#' If any of the arguments is of class \code{character}, the two arguments are concatenated and returned as string. Else the base implementation of \code{+} is called.
+#'
+#' @param e1 LHS
+#' @param e2 RHS
 #'
 #' @return A string or numeric.
 #' @export
@@ -14,9 +14,11 @@
 #' {
 #' 1+1
 #' "a"+"b"
+#' "a"+1+2
+#' 1+2+"a"
 #' }
 `+` <- function(e1, e2){
-  if(is.character(e1)){
+  if(is.character(c(e1, e2))){
     paste0(e1, e2)
   } else {
     .Primitive("+")(e1, e2)
@@ -24,5 +26,5 @@
 }
 
 
-  
+
 
