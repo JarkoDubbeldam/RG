b8142e717e1b4e02f19a6e5ec95c248a8eaf6481
JarkoDubbeldam <jarkodubbeldam@gmail.com>
2017-07-20 17:47:09 +0200

    Added documentation for splitStrings. Also removed an enthousiastic '=' from the function.

--- R/splitStrings.R
+++ R/splitStrings.R
@@ -1,5 +1,5 @@
 #' Split a vector into single characters or digits
-#' 
+#'
 #' Takes a string and returns all separated characters. If the input is numeric, the input is split into its digits and returned as numerics.
 #'
 #' @param x Vector to split. Can be numeric or character.
@@ -13,7 +13,7 @@
 #' s(c("bla", "bla"))
 #' s(155)
 #' s(c(151, 239))
-s <- function(x, on == ""){
+s <- function(x, on = ""){
   Load()
   num <- is.numeric(x)
   split <- strsplit(as.character(x), on)
